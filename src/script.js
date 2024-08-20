774108e5780199e3065ed9e89325335339a6aac4
Muzhgan Shams <muzhgan_shams@yahoo.com>
2024-08-20 23:46:37 +0500

    Inject API from JS

--- src/script.js
+++ src/script.js
@@ -16,6 +16,8 @@ function changeWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   tempElement.innerHTML = Math.round(temperature);
+
+  getForecast(response.data.city);
 }
 function formatDate(date) {
   let minutes = date.getMinutes();
@@ -48,7 +50,38 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+
+function getForecast(city) {
+  let apiKey = 'o1214355d46e610e0665f9f2cae5tadb';
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  let days = ['Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  let forecastHtml = '';
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+   <div class="weather-forecast-day">
+   <div class="weather-forecast-date">${day}</div>
+   <div class="weather-forecast-icon">🌤️</div>
+   <div class="weather-forecast-temperatures">
+   <div class="weather-forecast-temperature">
+   <strong>15º</strong>
+   </div>
+   <div class="weather-forecast-temperature">9º</div>
+   </div>
+   </div>`;
+  });
+  let forecastElement = document.querySelector('#forecast');
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector('#search-form');
 searchFormElement.addEventListener('submit', handleSearchSubmit);
 
 searchCity('Kabul');
+displayForecast();
