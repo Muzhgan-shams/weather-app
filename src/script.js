bb29abeab03ecf7c1a4caecd7d44e86e1971fe5e
Muzhgan Shams <muzhgan_shams@yahoo.com>
2024-08-20 17:08:47 +0500

    Add search functionality using API

--- src/script.js
+++ src/script.js
@@ -1,8 +1,24 @@
+function changeWeather(response) {
+  let tempElement = document.querySelector('#temp');
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector('#city-name');
+  cityElement.innerHTML = response.data.city;
+  tempElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = 'o1214355d46e610e0665f9f2cae5tadb';
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(changeWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector('#search-input');
-  let cityElement = document.querySelector('#place-name');
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector('#search-form');
 searchFormElement.addEventListener('submit', handleSearchSubmit);
+
+searchCity('Kabul');
